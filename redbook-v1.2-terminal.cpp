dd2c8f59a5f64c4f5425374b6f1ba44c8181b56a
AP1028 <105597219+AP1028@users.noreply.github.com>
2022-05-16 00:19:04 +0800

    Add files via upload

---
+++ redbook-v1.2-terminal.cpp
@@ -0,0 +1,97 @@
+#include <iostream>
+#define start 11
+int mcnt=start;
+char answ[100];
+using namespace std;
+void output(char ans,string type)
+{
+	if(type=="cat")
+	{
+		cout<<mcnt-start+1<<". 小猫"<<mcnt<<"("<<ans<<") [单选题] (1分)\n";
+		for(char i='A';i<='K';i++)
+		{
+			cout<<i<<endl;
+		}
+	}
+	else if(type=="cloze")
+	{
+		cout<<mcnt-start+1<<". 完型"<<mcnt<<"("<<ans<<") [单选题] (1分)\n";
+		for(char i='A';i<='D';i++)
+		{
+			cout<<i<<endl;
+		}
+	}
+	else if(type=="reading")
+	{
+		cout<<mcnt-start+1<<". 阅读"<<mcnt<<"("<<ans<<") [单选题] (2分)\n";
+		for(char i='A';i<='D';i++)
+		{
+			cout<<i<<endl;
+		}
+	}
+	else if(type=="6in4")
+	{
+		cout<<mcnt-start+1<<". 六选四"<<mcnt<<"("<<ans<<") [单选题] (2分)\n";
+		for(char i='A';i<='F';i++)
+		{
+			cout<<i<<endl;
+		}
+	}
+	else
+	{
+		cout<<"ERROR\n";
+	}
+	cout<<endl;
+	mcnt++;
+}
+int main()
+{
+	//freopen("answer.txt","r",stdin);
+	//freopen("output.txt","w",stdout);  
+	cout<<"请输入答案>";
+	for(int i=1;i<=33;i++)
+	{
+		if(i<=10)
+		{
+			char temp='!';
+			cin>>temp;
+			if(temp>=97)
+			{
+				temp=temp-32;
+			} 
+			output(temp,"cat");
+		}
+		else if(i<=25)
+		{
+			char temp='!';
+			cin>>temp;
+			if(temp>=97)
+			{
+				temp=temp-32;
+			} 
+			output(temp,"cloze");
+		}
+		else if(i<=29)
+		{
+			char temp='!';
+			cin>>temp;
+			if(temp>=97)
+			{
+				temp=temp-32;
+			} 
+			output(temp,"reading");
+		}
+		else
+		{
+			char temp='!';
+			cin>>temp;
+			if(temp>=97)
+			{
+				temp=temp-32;
+			} 
+			output(temp,"6in4");
+		}
+	} 
+	cout<<"\n\n#请直接将内容从命令行复制走\n#不要忘记设置姓名!\n";
+	return 0;
+}
